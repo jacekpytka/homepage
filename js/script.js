773b44bf755c50fa4c720cf83c58a83df07115c4
Jacek Pytka <jacekpytka@gmail.com>
2021-04-29 10:41:51 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const hideButton = document.querySelector(".hideButton");
+const imageJacek = document.querySelector(".imageJacek");
+const hidePhotoCss = document.querySelector(".hidePhotoCss");
+
+
+hideButton.addEventListener("click", () => {
+    imageJacek.classList.toggle("hidePhotoCss");
+    hideButton.innerText = imageJacek.classList.contains("hidePhotoCss") ? "Pokaż zdjęcie" : "Ukryj zdjęcie";
+});
+
+
+
+
