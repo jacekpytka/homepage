72cfc1eda8966d39b90b22342778442c1df5a345
jacekpytka <83397945+jacekpytka@users.noreply.github.com>
2021-05-12 10:57:58 +0200

    js details

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
-const hideButton = document.querySelector(".hideButton");
+{const hideButton = document.querySelector(".hideButton");
 const imageJacek = document.querySelector(".imageJacek");
 const hidePhotoCss = document.querySelector(".hidePhotoCss");
-
-
 hideButton.addEventListener("click", () => {
     imageJacek.classList.toggle("hidePhotoCss");
     hideButton.innerText = imageJacek.classList.contains("hidePhotoCss") ? "Pokaż zdjęcie" : "Ukryj zdjęcie";
-});
+})};
 
 
 
